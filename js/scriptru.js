7d2ceccf9b963f086b5a08086ac455c6907a39d4
JolyThesign <80939524+JolyTh3Kidd@users.noreply.github.com>
2022-02-19 13:21:15 +0600

    Add files via upload

---
+++ js/scriptru.js
@@ -0,0 +1,16 @@
+document.getElementById('AboutRU').onclick = function() {
+    var i = 'Курмет Рахатов - Обо мне';
+    document.title = i;
+}
+document.getElementById('WorksRU').onclick = function() {
+    var i = 'Курмет Рахатов - Работы';
+    document.title = i;
+}
+document.getElementById('ContactsRU').onclick = function() {
+    var i = 'Курмет Рахатов - Контакты';
+    document.title = i;
+}
+document.getElementById('portfolioRU').onclick = function() {
+    var i = 'Курмет Рахатов - Портфолио';
+    document.title = i;
+}
